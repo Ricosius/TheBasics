fb8943fbe7696ee42c8d69093c71f5518de64add
Rico Kolditz <ricokolditz@hotmail.com>
2014-03-10 12:21:39 +0100

    Sounds toegevoegd aan splash

--- src/com/ricosius/thebasic/MainActivity.java
+++ src/com/ricosius/thebasic/MainActivity.java
@@ -1,16 +1,21 @@
 package com.ricosius.thebasic;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
-
+	
+	MediaPlayer logoMusic;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
+        
+        logoMusic = MediaPlayer.create(MainActivity.this, R.raw.southafrica );
+        logoMusic.start();
         Thread logoTimer = new Thread(){
         	public void run(){
         		try{
@@ -29,9 +34,17 @@ public class MainActivity extends Activity {
         };
         logoTimer.start();
     }
-
+    
 
     @Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+		logoMusic.release();
+	}
+
+
+	@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
